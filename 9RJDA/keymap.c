8fadbf80efd8d13dbb750170f5a3e1a180515107
Regnareb <kun_fu_man@hotmail.com>
2024-11-13 02:14:09 +0100

    On GAME layout: disable autoshift and activate SOCD

--- 9RJDA/keymap.c
+++ 9RJDA/keymap.c
@@ -8,10 +8,18 @@
 
 // https://getreuer.info/posts/keyboards/socd-cleaner/index.html
 socd_cleaner_t socd_h = {{KC_A, KC_D}, SOCD_CLEANER_LAST};
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//   socd_cleaner_enabled = IS_LAYER_ON_STATE(state, GAME);
-//   return state;
-// }
+layer_state_t layer_state_set_user(layer_state_t state) {
+  socd_cleaner_enabled = IS_LAYER_ON_STATE(state, GAME);
+  switch (get_highest_layer(state)) {
+    case GAME:
+      autoshift_disable();
+      break;
+    default:
+      autoshift_enable();
+      break;
+  }
+  return state;
+}
 
 
 enum custom_keycodes {
